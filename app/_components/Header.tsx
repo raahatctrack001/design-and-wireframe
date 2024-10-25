717c6b26f67bf272438b8acc368ce1406edfda22
Raahat Khan <raahatctrack001.rc@gmail.com>
2024-10-25 21:32:12 +0530

    add authentication using kinde.com

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -1,3 +1,5 @@
+import { LoginLink, RegisterLink } from "@kinde-oss/kinde-auth-nextjs/components";
+
 export default function Header(){
     return <header className="bg-black">
     <div className="mx-auto flex h-16 max-w-screen-xl items-center gap-8 px-4 sm:px-6 lg:px-8">
@@ -42,19 +44,19 @@ export default function Header(){
   
         <div className="flex items-center gap-4">
           <div className="sm:flex sm:gap-4">
-            <a
+            <div
               className="block rounded-md px-5 py-2.5 text-sm font-medium text-white transition hover:bg-gray-500"
-              href="#"
+            //   href="#"
             >
-              Login
-            </a>
+                <LoginLink> Login </LoginLink>
+            </div>
   
-            <a
+            <div
               className="hidden rounded-md bg-gray-100 px-5 py-2.5 text-sm font-medium text-black transition hover:text-gray-800/75 sm:block"
-              href="#"
+            //   href="#"
             >
-              Register
-            </a>
+              <RegisterLink> Register </RegisterLink>
+            </div>
           </div>
   
           <button
